cb2b6c4ddf00c6229e9984a73d8f85985fa584cf
Kevin Eder <kevin.eder@gmail.com>
2013-05-02 16:31:45 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchdork::Application.config.secret_token = '710eecc68429a9202419a6097a866758c9c11237a9e456873d80302a6ba4d892874d0068002d566e39006b89e3ea2f4a9aa4f7ccb83f00e6093ca9cffc76315a'
