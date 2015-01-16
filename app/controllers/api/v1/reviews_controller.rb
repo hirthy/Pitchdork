c4ddd9400e68470d7f0e4602c2f13a6cfe660a90
Mike Hirth <hirthy@gmail.com>
2015-01-16 01:37:12 -0500

    Remove sorting

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -5,9 +5,9 @@ module Api::V1
     # GET /v1/reviews
     def index
       if params[:artist]
-        @results = Review.where(:publish_date.exists => true, :genre.exists => true, :artist => params[:artist]).order_by(:publish_date => 'asc')
+        @results = Review.where(:publish_date.exists => true, :genre.exists => true, :artist => params[:artist])
       else
-        @results = Review.where(:publish_date.exists => true, :genre.exists => true).order_by(:publish_date => 'asc')
+        @results = Review.where(:publish_date.exists => true, :genre.exists => true)
       end
 
       render json: @results
