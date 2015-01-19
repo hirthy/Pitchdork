a8f110babb0f019727813b7cafb58d8278295a3f
Mike Hirth <hirthy@gmail.com>
2015-01-18 20:08:52 -0500

    Remove more fields to speed up query

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -5,9 +5,9 @@ module Api::V1
     # GET /v1/reviews
     def index
       if params[:artist]
-        @results = Review.only(:artist, :score, :album_title, :genre, :publish_date).where(:publish_date.exists => true, :genre.exists => true, :artist => params[:artist])
+        @results = Review.without(:_id, :url, :html, :body, :reviewer_name, :album_image, :spotify_artist_id, :spotify_artist_popularity, :spotify_album_popularity, :spotify_album_id, :spotify_release_date, :spotify_track_ids, :spotify_genres, :spotify_last_check, :last_fm_tags, :album_image_path).where(:publish_date.exists => true, :genre.exists => true, :artist => params[:artist]).entries.map {|d| d.as_document}
       else
-        @results = Review.only(:artist, :score, :album_title, :genre, :publish_date).where(:publish_date.exists => true, :genre.exists => true)
+        @results = Review.without(:_id, :url, :html, :body, :reviewer_name, :album_image, :spotify_artist_id, :spotify_artist_popularity, :spotify_album_popularity, :spotify_album_id, :spotify_release_date, :spotify_track_ids, :spotify_genres, :spotify_last_check, :last_fm_tags, :album_image_path).where(:publish_date.exists => true, :genre.exists => true).entries.map {|d| d.as_document}
       end
 
       render json: @results
